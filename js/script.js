82bbc824f5667f1a6ee447bc22ecc438f7b347a3
Rastislav Kobac <rastislav.kobac@gmail.com>
2017-09-23 23:28:13 +0200

    Master version

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function myMove() {
+  var elem = document.getElementById("animate");   
+  var pos = 0;
+  var id = setInterval(frame, 5);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos++; 
+      elem.style = 'animation: rotation 1s infinite linear;';
+   
+      //elem.style.left = pos + 'px'; 
+    }
+  }
+}
